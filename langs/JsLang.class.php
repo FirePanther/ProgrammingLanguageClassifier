a717c7148eb0f75e428a429e5e08370f3d3e29cc
FirePanther <git@su.at>
2016-12-17 17:48:26 +0100

    ignore html attribs, oop (es6 stuff), ext php vars

--- langs/JsLang.class.php
+++ langs/JsLang.class.php
@@ -14,11 +14,11 @@ class JsLang extends Lang {
 	
 	public function run() {
 		$this->rest = ' '.$this->code.' ';
+		$this->keys['demerit'] = -2;
 		
 		$this->keys['stringsLen'] = 0;
 		$this->keys['commentsLen'] = 0;
 		$this->keys['variablesLen'] = 0;
-		$this->keys['demerit'] = 0;
 		
 		// escapes
 		$this->rest = preg_replace('~\\\\.~', '', $this->rest);
@@ -29,14 +29,44 @@ class JsLang extends Lang {
 				$this->keys['demerit'] += strlen($m[0]);
 				return '';
 			}, $this->rest);
+			$onEventsRegex = '('.implode('|', $this->keywords(true)).')';
+			$this->rest = preg_replace_callback('~ '.$onEventsRegex.'=("|\')[^\1]+\1~i', function($m) {
+				$this->keys['demerit'] += strlen($m[0]);
+				return '';
+			}, $this->rest);
+			$this->rest = preg_replace_callback('~ '.$onEventsRegex.'=[^\s>]+~i', function($m) {
+				$this->keys['demerit'] += strlen($m[0]);
+				return '';
+			}, $this->rest);
+			// curly brace whitespace
+			$this->rest = preg_replace_callback('~\s+\{~i', function($m) {
+				$this->keys['demerit'] += strlen($m[0]) - 1;
+				return '{';
+			}, $this->rest);
+			// curly braces validity
+			$this->rest = preg_replace_callback('~(\W(else|try|finally|in|return|class|class\s+\w+|extends\s+\w+|implements\s+\w+)\s*)\{~i', function($m) {
+				// make it valid for the next check
+				$this->keys['demerit'] -= 2;
+				return $m[1].'(){';
+			}, $this->rest);
+			// - the ">" enables a bit of ecmascript 6 validity
+			if (preg_match('~[^,\{\(\)\[\=\|\&\!\?\:>]\{~', $this->rest)) {
+				$this->errors = true;
+				return;
+			}
 			// whitespaces
-			$this->rest = str_replace(["\r\n", "\r"], "\n", $this->rest);
-			$this->rest = preg_replace('~\s+~m', ' ', $this->rest);
+			$this->rest = preg_replace_callback('~\s+~m', function($m) {
+				$this->keys['demerit'] += strlen($m[0]) - 1;
+				return ' ';
+			}, $this->rest);
 			// regex
 			$this->rest = preg_replace('~/[^/]+/[gim]+~i', ' ', $this->rest);
-			// functions
-			$this->rest = preg_replace('~(\W)function\s+\w+\s*\(~i', '', $this->rest);
-			$this->rest = preg_replace('~(\W)\w+\([^\)]*\)\s*(;|\{)~i', '', $this->rest);
+			// function declarations and anonymous functions
+			$this->rest = preg_replace_callback('~(\W)function(\s+\w+)?\s*\(~i', function($m) {
+				$this->keys['keywords']++;
+				$this->keys['keywordsLen'] += 8;
+				return $m[1];
+			}, $this->rest);
 			// keywords
 			$keywords = $this->keywords();
 			foreach ($keywords as $k) {
@@ -46,6 +76,14 @@ class JsLang extends Lang {
 					return '';
 				}, $this->rest);
 			}
+			// function calls
+			$this->rest = preg_replace_callback('~(\W)(\w+)\([^\)]*\)\s*(;|\{)~i', function($m) {
+				if ($m[2] == 'constructor') {
+					$this->keys['keywords']++;
+					$this->keys['keywordsLen'] += strlen($m[2]);
+				}
+				return $m[1];
+			}, $this->rest);
 			// special chars
 			$this->rest = preg_replace('~(/|\*|=|;|\(|\)|\[|\]|\{|\}|\&|\|\||,|\?|\:|\!|<|>|\+|\-|\d*\.\d+e?|\d+\.\d*e?|0x\d+|\d+e?)~i', ' ', $this->rest);
 			// objects, arrays, variables
@@ -55,19 +93,24 @@ class JsLang extends Lang {
 				$restBackup = $this->rest;
 				$this->rest = preg_replace('~\[[^\]]+\]~i', '', $this->rest);
 			} while($restBackup !== $this->rest);
-			$this->keys['allVarsPhpValid'] = 0; // no variables at all
+			$this->keys['validPhpVarsLen'] = 0; // no variables at all
 			$this->rest = preg_replace_callback('~([a-z\$_][a-z0-9\$_]*)(\.[a-z\$_][a-z0-9\$_]*)?~i', function($m) {
-				if ($this->keys['allVarsPhpValid'] === 0) $this->keys['allVarsPhpValid'] = 1;
 				$this->keys['variablesLen'] += strlen($m[0]);
-				if (strpos($m[0], '(') === false) {
-					if (!preg_match('~^\$[a-z_]\w*($|\.)~', $m[0])) { // php invalid
-						$this->set('allVarsPhpValid', -1);
-					}
+				
+				if (strpos($m[0], '(') === false && !(!isset($m[2]) && in_array($m[1], [
+					'echo', 'return', 'break', 'case', 'continue', 'var', 'self', 'const', 'print', 'global', 'use','abstract',
+					'class','declare','extends','interface','namespace'
+				]))) {
+					if (preg_match('~^\$[a-z_]\w*($|\.)~', $m[0])) $this->keys['validPhpVarsLen'] += strlen($m[0]);
+					else $this->keys['validPhpVarsLen'] += strlen($m[0]);
 				}
 				return '';
 			}, $this->rest);
 			// remove whitespace
-			$this->rest = str_replace(' ', '', $this->rest);
+			$this->rest = preg_replace_callback('~\s+~', function($m) {
+				$this->keys['demerit'] += strlen($m[0]);
+				return '';
+			}, $this->rest);
 		} else {
 			$this->errors = true;
 		}
@@ -135,8 +178,19 @@ class JsLang extends Lang {
 	/**
 	 * js keywords (system functions, constants, ...)
 	 */
-	private function keywords() {
-		return [
+	private function keywords($justOnEvents = false) {
+		$onEvents = ['onclick', 'oncontextmenu', 'ondblclick', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove',
+			'onmouseover', 'onmouseout', 'onmouseup', 'onkeydown', 'onkeypress', 'onkeyup', 'onabort', 'onbeforeunload',
+			'onerror', 'onhashchange', 'onload', 'onpageshow', 'onpagehide', 'onresize', 'onscroll', 'onunload', 'onblur',
+			'onchange', 'onfocus', 'onfocusin', 'onfocusout', 'oninput', 'oninvalid', 'onreset', 'onsearch', 'onselect',
+			'onsubmit', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop',
+			'oncopy', 'oncut', 'onpaste', 'onafterprint', 'onbeforeprint', 'onabort', 'oncanplay', 'oncanplaythrough',
+			'ondurationchange', 'onemptied', 'onended', 'onerror', 'onloadeddata', 'onloadedmetadata', 'onloadstart',
+			'onpause', 'onplay', 'onplaying', 'onprogress', 'onratechange', 'onseeked', 'onseeking', 'onstalled', 'onsuspend',
+			'ontimeupdate', 'onvolumechange', 'onwaiting'];
+		if ($justOnEvents) return $onEvents;
+		
+		return array_merge([
 			'break','case','catch','const','continue',
 			'default','delete','do',
 			'else',
@@ -160,7 +214,7 @@ class JsLang extends Lang {
 			'final','float','implements','import','int','interface','long','native',
 			'short','super','synchronized','throws','transient','volatile',
 			
-			'window', 'document', 'console',
+			'window','document','console','constructor',
 			
 			'alert', 'back', 'blur', 'cancelIdleCallback', 'captureEvents', 'clearImmediate', 'close',
 			'confirm', 'disableExternalCapture', 'dispatchEvent', 'dump', 'enableExternalCapture', 'find',
@@ -172,20 +226,12 @@ class JsLang extends Lang {
 			'setImmediate', 'setResizable', 'showModalDialog', 'sizeToContent', 'stop', 'updateCommands',
 			'mozRequestAnimationFrame',
 			
-			'onclick', 'oncontextmenu', 'ondblclick', 'onmousedown', 'onmouseenter', 'onmouseleave', 'onmousemove',
-			'onmouseover', 'onmouseout', 'onmouseup', 'onkeydown', 'onkeypress', 'onkeyup', 'onabort', 'onbeforeunload',
-			'onerror', 'onhashchange', 'onload', 'onpageshow', 'onpagehide', 'onresize', 'onscroll', 'onunload', 'onblur',
-			'onchange', 'onfocus', 'onfocusin', 'onfocusout', 'oninput', 'oninvalid', 'onreset', 'onsearch', 'onselect',
-			'onsubmit', 'ondrag', 'ondragend', 'ondragenter', 'ondragleave', 'ondragover', 'ondragstart', 'ondrop',
-			'oncopy', 'oncut', 'onpaste', 'onafterprint', 'onbeforeprint', 'onabort', 'oncanplay', 'oncanplaythrough',
-			'ondurationchange', 'onemptied', 'onended', 'onerror', 'onloadeddata', 'onloadedmetadata', 'onloadstart',
-			'onpause', 'onplay', 'onplaying', 'onprogress', 'onratechange', 'onseeked', 'onseeking', 'onstalled', 'onsuspend',
-			'ontimeupdate', 'onvolumechange', 'onwaiting', 'animationend', 'animationiteration', 'animationstart',
+			'animationend', 'animationiteration', 'animationstart',
 			'transitionend', 'onerror', 'onmessage', 'onopen', 'onmessage', 'onmousewheel', 'ononline', 'onoffline',
 			'onpopstate', 'onshow', 'onstorage', 'ontoggle', 'onwheel', 'ontouchcancel', 'ontouchend', 'ontouchmove',
 			'ontouchstart', 'CAPTURING_PHASE', 'AT_TARGET', 'BUBBLING_PHASE', 'bubbles', 'cancelable', 'currentTarget',
 			'defaultPrevented', 'eventPhase', 'isTrusted', 'target', 'timeStamp', 'type', 'view', 'preventDefault',
 			'stopImmediatePropagation', 'stopPropagation'
-		];
+		], $onEvents);
 	}
 }
